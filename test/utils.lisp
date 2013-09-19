92ebd13d7f0b97dfcd959b57f8fd9ba979a92840
Marc Kuo <kuomarc2@gmail.com>
2013-09-19 16:24:07 -0700

    Alist-count util

--- test/utils.lisp
+++ test/utils.lisp
@@ -2,6 +2,8 @@
 
 (in-package #:cl-oneliner)
 
+;;; Simple hash count
+
 (define-test hashcount
   "Test the hashcount util"
   (:tag :util)
@@ -25,3 +27,14 @@
   (assert-equal "this" (most-frequent (hashcount "this is this test")))
   (assert-equal "is" (most-frequent (hashcount "Is this is this test is?")))
   (assert-equal "test" (most-frequent (hashcount "TEST test, testing; test!"))))
+
+;;; Simple alist count
+
+(define-test alist-count
+  "Test the alist-count util"
+  (:tag :util)
+  (assert-equal 1 (get-count "this" (alist-count "this is a test")))
+  (assert-equal 2 (get-count "this" (alist-count "this is this")))
+  (assert-equal 3 (get-count "this" (alist-count "not this but this is this")))
+  (assert-equal 4 (get-count "this" (alist-count "this THIS This this")))
+  (assert-equal 5 (get-count "this" (alist-count "this, THIS This. this; THIS!"))))
--- utils.lisp
+++ utils.lisp
@@ -20,3 +20,47 @@
 (defun most-frequent (hash)
   (let ((alist (hash-to-alist hash)))
     (car (reduce (lambda (x y) (if (> (cdr x) (cdr y)) x y )) alist))))
+
+
+;;; Custom data structure for hash count
+;;; An Alist where the value is another Alist, which holds count and next
+;;; -----------------
+;;; ((wordA (count . 3)
+;;;         (next (wordB . 3)))
+;;;  (wordB (count . 5)
+;;;         (next (wordC . 2)
+;;;               (wordD . 3))))
+;;; -----------------
+;;; This example shows a hashcount of words, and the word occurance of the words after the word
+;;;  - wordA occured 3 times, always followed by wordB
+;;;  - wordB occured 5 times, two times followed by wordC and three times by wordD
+
+(defun alist-count (string)
+  "Given a string, return an Alist counting the words"
+  (let ((words (words-sorted string)))
+    (labels ((iter (words alist &optional (count 1))
+               (if (null words) alist
+                   (let ((word (car words))
+                         (next (cadr words)))
+                     (if (string= word next)
+                         (iter (cdr words) alist (1+ count))
+                         (iter (cdr words) (acons word `((count . ,count)) alist)))))))
+      (iter words '()))))
+
+(defun simplify-word (word)
+  "Given a word, put it to lower-case and remove all symbols"
+  (string-downcase (regex-replace-all "[!-@]" word "")))
+
+(defun words-sorted (sentence)
+  (sort
+   (mapcar #'simplify-word
+           (split-sequence #\Space sentence))
+   #'string<))
+
+(defun aval (key alist)
+  "Given alist and key, return value"
+  (cdr (assoc key alist :test #'equal)))
+
+(defun get-count (word alist-count)
+  "Given a word and an alist-count, return number of occurence"
+  (cdar (aval word alist-count)))
