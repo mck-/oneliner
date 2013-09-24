199cc25885622d1f221ca7a34542894120b2f2e4
Marc Kuo <kuomarc2@gmail.com>
2013-09-23 20:23:44 -0700

    remove legacy hashcount methods and tests

--- test/utils.lisp
+++ test/utils.lisp
@@ -2,34 +2,6 @@
 
 (in-package #:cl-oneliner)
 
-;;; Simple hash count
-
-(define-test hashcount
-  "Test the hashcount util"
-  (:tag :util)
-  (assert-equal 1 (gethash "this" (hashcount "this is a test")))
-  (assert-equal 2 (gethash "this" (hashcount "this is this")))
-  (assert-equal 3 (gethash "this" (hashcount "not this but this is this")))
-  (assert-equal 4 (gethash "this" (hashcount "this THIS This this")))
-  (assert-equal 5 (gethash "this" (hashcount "this, THIS This. this; THIS!"))))
-
-(define-test hash-to-alist
-  "Convert a hash to an alist"
-  (:tag :util)
-  (let ((strhash (make-hash-table :test 'equal)))
-    (setf (gethash "hello" strhash) 2)
-    (setf (gethash "world" strhash) 3)
-    (setf (gethash "this" strhash) 10)
-    (assert-equal '(("hello" . 2) ("world" . 3) ("this" . 10)) (hash-to-alist strhash))))
-
-(define-test highest-occurence
-  (:tag :util)
-  (assert-equal "this" (most-frequent (hashcount "this is this test")))
-  (assert-equal "is" (most-frequent (hashcount "Is this is this test is?")))
-  (assert-equal "test" (most-frequent (hashcount "TEST test, testing; test!"))))
-
-;;; Simple alist count
-
 (define-test alist-count
   "Test the alist-count util"
   (:tag :util)
--- utils.lisp
+++ utils.lisp
@@ -2,25 +2,6 @@
 
 (in-package #:cl-oneliner)
 
-(defun hashcount (string)
-  (let ((hc (make-hash-table :test 'equal))
-        (words (split-sequence #\Space string)))
-    (dolist (w words)
-      (let ((low (string-downcase (regex-replace-all "[!-@]" w ""))))
-        (if (gethash low hc)
-            (incf (gethash low hc))
-            (setf (gethash low hc) 1))))
-    hc))
-
-(defun hash-to-alist (hash)
-  (loop for val being the hash-values of hash
-     and key being the hash-keys of hash
-     collect (cons key val)))
-
-(defun most-frequent (hash)
-  (let ((alist (hash-to-alist hash)))
-    (car (reduce (lambda (x y) (if (> (cdr x) (cdr y)) x y )) alist))))
-
 ;;; Custom data structure for hash count
 ;;; An Alist where the value is another Alist, which holds count and next
 ;;; -----------------
